9cc038181515ba669fa81fcc0e5166baad0eb6f4
roma1n <golubtsowroman@gmail.com>
2023-10-02 19:17:02 +0300

    Add precommit checks

--- mlops_practice/main.py
+++ mlops_practice/main.py
@@ -1,6 +1,7 @@
 import collections
-from sklearn.datasets import load_digits
+
 import torch
+from sklearn.datasets import load_digits
 from torch import nn
 from uniplot import plot
 
@@ -72,8 +73,8 @@ class ClassifierTrainer:
         losses = []
         self.model.train(True)
         for i in range(0, X_train.shape[0], batch_size):
-            X_batch = X_train[i: i + batch_size]
-            y_batch = y_train[i: i + batch_size]
+            X_batch = X_train[i : i + batch_size]
+            y_batch = y_train[i : i + batch_size]
 
             logits = self.model(X_batch)
             loss = self.criterion(logits, y_batch)
@@ -83,8 +84,8 @@ class ClassifierTrainer:
             losses.append(loss.item())
 
         loss = sum(losses) / len(losses)
-        print(f'Train loss: {loss:.4f}')
-        self.history['train_loss'].append(loss)
+        print(f"Train loss: {loss:.4f}")
+        self.history["train_loss"].append(loss)
 
     def process_val(self, dataset, batch_size):
         X_val, y_val = dataset
@@ -93,8 +94,8 @@ class ClassifierTrainer:
         self.model.train(False)
         with torch.no_grad():
             for i in range(0, X_val.shape[0], BATCH_SIZE):
-                X_batch = X_val[i: i + BATCH_SIZE]
-                y_batch = y_val[i: i + BATCH_SIZE]
+                X_batch = X_val[i : i + BATCH_SIZE]
+                y_batch = y_val[i : i + BATCH_SIZE]
 
                 logits = self.model(X_batch)
                 accuracy.extend((logits.argmax(axis=1) == y_batch).tolist())
@@ -103,45 +104,49 @@ class ClassifierTrainer:
 
         loss = sum(losses) / len(losses)
         accuracy = sum(accuracy) / len(accuracy)
-        print(f'Validation loss: {loss:.4f}')
-        print(f'Validation accuracy: {accuracy:.4f}')
-        self.history['val_loss'].append(loss)
-        self.history['val_accuracy'].append(accuracy)
-
+        print(f"Validation loss: {loss:.4f}")
+        print(f"Validation accuracy: {accuracy:.4f}")
+        self.history["val_loss"].append(loss)
+        self.history["val_accuracy"].append(accuracy)
 
     def fit(self, train_dataset, val_dataset, n_epoch=N_EPOCH, batch_size=BATCH_SIZE):
-        print('\n=== Validate model before training ===')
+        print("\n=== Validate model before training ===")
         self.process_val(val_dataset, batch_size)
 
         for epoch in range(1, 1 + N_EPOCH):
-            print(f'\n=== Epoch {epoch} of {N_EPOCH} ===')
+            print(f"\n=== Epoch {epoch} of {N_EPOCH} ===")
             self.process_train(train_dataset, batch_size)
             self.process_val(val_dataset, batch_size)
-        print ('Model fitted!')
+        print("Model fitted!")
 
     def plot(self):
-        print('\n=== Val accuracy by epoch ===')
-        plot(self.history['val_accuracy'])
+        print("\n=== Val accuracy by epoch ===")
+        plot(self.history["val_accuracy"])
 
-        print('\n=== Train & val losses by epoch ===')
-        plot([self.history['val_loss'], self.history['train_loss']])
+        print("\n=== Train & val losses by epoch ===")
+        plot([self.history["val_loss"], self.history["train_loss"]])
 
 
 def get_train_val_datasets():
-    print('Loading dataset')
+    print("Loading dataset")
     X, y = load_digits(return_X_y=True)
     X = torch.tensor(X).reshape(X.shape[0], 1, 8, 8).float()
     y = torch.tensor(y)
 
-    print('Sample from dataset')
-    print('\n'.join(map(lambda line: ''.join(map(lambda elem: str(elem.item()), line)), (X[0, 0] > 0).long())))
-    print(f'class: {y[0].item()}\n')
-
+    print("Sample from dataset")
+    print(
+        "\n".join(
+            map(
+                lambda line: "".join(map(lambda elem: str(elem.item()), line)), (X[0, 0] > 0).long()
+            )
+        )
+    )
+    print(f"class: {y[0].item()}\n")
 
     train_size = int(X.shape[0] * TRAIN_RATIO)
     X_train, X_val = X[:train_size], X[train_size:]
     y_train, y_val = y[:train_size], y[train_size:]
-    print(f'Train samples: {X_train.shape[0]}; val samples: {X_val.shape[0]}')
+    print(f"Train samples: {X_train.shape[0]}; val samples: {X_val.shape[0]}")
 
     return (X_train, y_train), (X_val, y_val)
 
@@ -159,5 +164,5 @@ def main():
     trainer.plot()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
